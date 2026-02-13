af2f57bfb9eb048380f0dd9badcbc15800742035
Sky Bristol <skybristol@gmail.com>
2026-02-13 16:30:46 -0700

    Fixed formatting issue

--- tests/test_docs_build.py
+++ tests/test_docs_build.py
@@ -11,4 +11,7 @@ def test_mkdocs_build_strict(tmp_path):
         pytest.skip("mkdocs is not installed. Run: poetry install --with docs")
 
     site_dir = tmp_path / "site"
-    subprocess.run([mkdocs, "build", "--strict", "--site-dir", str(site_dir)], check=True)
+    subprocess.run(
+        [mkdocs, "build", "--strict", "--site-dir", str(site_dir)],
+        check=True,
+    )
